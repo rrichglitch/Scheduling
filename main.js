434edbcccf666e7b372607f8dd5ebd51a9369220
roary <rrichglitch@gmail.com>
2023-06-22 15:46:29 -0400

    reinstalled electron

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 // Imports
 const { app, BrowserWindow } = require('electron')
 
+const devMode = process.env.NODE_ENV !== 'production'
+
 // Functions
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
+    width: 800 + (devMode ? 400 : 0),
     height: 600
   })
 
   win.loadFile('index.html')
+
+  if(devMode) win.webContents.openDevTools()
 }
 
 // Initialize
@@ -19,6 +23,14 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
+
+// Menu Template
+// const menu = {
+//   {
+
+//   }
+// }
+
 // Close
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
